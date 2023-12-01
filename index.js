032f419e47a18a1b00b668deebc643764216a6fc
Sajeda Akter <sajeda16akter@gmail.com>
2023-12-01 09:12:05 +0600

    'addToProduct-post-complete'

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const cors=require('cors')
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port =process.env.PORT || 5000
 
 
@@ -8,6 +10,36 @@ const port =process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nvo4nkj.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+ try{
+    await client.connect()
+    const addProductCollection=client.db('techhubdb').collection('addProduct')
+
+    // add to product in the database
+    app.post('/addProduct',(req,res)=>{
+        const add=req.body
+        const result=addProductCollection.insertOne(add);
+        res.send(result)
+    })
+ }
+ finally{
+
+ }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Welcome to techHub!')
